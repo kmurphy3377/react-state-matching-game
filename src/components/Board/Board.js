66e367c38a02747b77444d8fe629205c7cb491de
Kelly Murphy <kelly.murphy@bbc.co.uk>
2022-04-22 16:34:38 +0100

    Section 2: Hydrate the state

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import './Board.css';
+import React from "react";
+import "./Board.css";
+import Tile from "../Tile";
 
 const Board = (props) => {
+  const tileComponents = props.tiles.map((tile) => <Tile {...tile}/>);
 
   const gridConfig = {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-  }
+  };
 
+  return <div className="Board" style={gridConfig}>{tileComponents}</div>;
+};
 
-  return (
-    <div className='Board' style={gridConfig}>
-    </div>
-  )
-}
-
-export default Board
+export default Board;
